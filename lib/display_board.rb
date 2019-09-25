160668338d6b26728ae79870435ba2d9dc6f643b
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-25 18:10:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,5 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
-  ttt_board = ["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
-  ttt_board.each { |x| puts "#{x}\n" }
+  
 end
